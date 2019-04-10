b1284eb295965bbeb4a4f42067130e8345507a88
yurabraiko <yurabraiko@gmail.com>
2019-04-10 20:48:29 +0300

    draft

---
+++ src/katas/regression/regression.py
@@ -0,0 +1,24 @@
+from typing import Union, Callable, Tuple
+
+import pandas as pd
+
+
+
+#
+# class Regression:
+#     def __init__(self, data_source: DataSource, label_col_name: str) -> None:
+#         super().__init__()
+#         self.data_source = data_source
+#
+#     @staticmethod
+#     def split_df(df:pd.DataFrame, split_col_name: str) ->DataLabel:
+#         return (df[df.columns.drop(split_col_name)], df[[split_col_name]])
+#     #
+#     # def get_data_batch(self):
+#     #     if
+
+# train[train.columns.drop('default.payment.next.month')], train[['default.payment.next.month']
+
+class Regression:
+
+    def fit(self,):
